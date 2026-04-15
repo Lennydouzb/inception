6035da96dab8a9358edaeb499e8104bfa752d285
Lenny Desbouis <ldesboui@1A7.42angouleme.fr>
2026-04-15 22:05:48 +0200

    Commit

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -6,10 +6,10 @@ while ! mysqladmin ping --silent; do
     sleep 1
 done
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
 mariadb -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-mariadb -e "GRANT ALL PRIVILEGES ON '${DB_NAME}'.* TO '${DB_USER}'@'%' WITH GRANT OPTION;"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%' WITH GRANT OPTION;"
 mariadb -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root shutdown
-exec mysqld
+exec mysqld --user=mysqld 
