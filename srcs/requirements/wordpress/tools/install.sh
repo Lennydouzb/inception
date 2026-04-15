3c2a350d0d4df2f088f9852a9da53b35746af83a
Lenny Desbouis <ldesboui@1A7.42angouleme.fr>
2026-04-15 22:35:39 +0200

    Commit

--- srcs/requirements/wordpress/tools/install.sh
+++ srcs/requirements/wordpress/tools/install.sh
@@ -10,7 +10,7 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 
 	./wp-cli.phar core download --allow-root
 	./wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=mariadb --allow-root
-	./wp-cli.phar core install --url=$DOMAIN_NAME --title=inception --admin_user=$WP_USER --admin_pass=$WP_PASS --admin_email=$WP_MAIL --allow-root
+	./wp-cli.phar core install --url=$DOMAIN_NAME --title=inception --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=$WP_MAIL --allow-root
 	./wp-cli.phar user create $WP_USER2 $WP_MAIL2 --role=author --user_pass=$WP_PASS2 --allow-root
 	./wp-cli.phar user set-role $WP_USER2 editor --allow-root
 	./wp-cli.phar theme install astra --activate --allow-root
