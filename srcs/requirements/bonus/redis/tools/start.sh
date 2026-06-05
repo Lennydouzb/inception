3bbb664d3c5758735ae6ac808687a3a204ba6f55
Lenny Desbouis <ldesboui@2G3.42angouleme.fr>
2026-06-05 14:18:31 +0200

    Commit

---
+++ srcs/requirements/bonus/redis/tools/start.sh
@@ -0,0 +1,4 @@
+echo "maxmemory 256mb" >> /etc/redis/redis.conf
+echo "maxmemory-policy allkeys-lru" >> /etc/redis/redis.conf
+echo "requirepass" $REDIS_PASSWORD >> /etc/redis/redis.conf
+redis-server /etc/redis/redis.conf --protected-mode no
