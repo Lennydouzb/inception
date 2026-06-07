3aca16d9a0fda69f7a6b73d0b1619093a1476e0c
Lenny Desbouis <ldesboui@2G3.42angouleme.fr>
2026-06-07 15:55:17 +0200

    Commit

--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -5,10 +5,11 @@ listen=YES
 local_enable=YES
 write_enable=YES
 chroot_local_user=YES
-local_enable=YES
 pasv_enable=YES
 pasv_min_port=30000
 pasv_max_port=30009
+pasv_address=127.0.0.1
+pasv_addr_resolve=YES
 seccomp_sandbox=NO
 allow_writeable_chroot=YES" >> /etc/vsftpd.conf
 mkdir -p /var/run/vsftpd/empty
