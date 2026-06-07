11d6a05c4783ed18261f430b22d73e459b88b8af
Lenny Desbouis <ldesboui@2G3.42angouleme.fr>
2026-06-07 16:48:30 +0200

    Commit

--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -11,6 +11,7 @@ pasv_max_port=30009
 pasv_address=127.0.0.1
 pasv_addr_resolve=NO
 seccomp_sandbox=NO
+local_umask=006
 allow_writeable_chroot=YES" > /etc/vsftpd.conf
 mkdir -p /var/run/vsftpd/empty
 if ! id "$FTPUSER" &>/dev/null; then
