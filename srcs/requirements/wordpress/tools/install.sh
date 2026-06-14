203dd46efe9a52cafdd2e6ebfbb4071e54e7ae51
Lenny Desbouis <ldesboui@3A2.42angouleme.fr>
2026-06-14 16:59:33 +0200

    Commit

--- srcs/requirements/wordpress/tools/install.sh
+++ srcs/requirements/wordpress/tools/install.sh
@@ -17,6 +17,7 @@ if [ ! -f /var/www/html/wp-config.php ]; then
     ./wp-cli.phar config set WP_REDIS_PORT 6379 --raw --type=constant --allow-root
     ./wp-cli.phar plugin install redis-cache --activate --allow-root
     ./wp-cli.phar redis enable --allow-root
+fi
 chown -R www-data:www-data /var/www/html
 chmod -R 775 /var/www/html
 exec php-fpm8.2 -F
